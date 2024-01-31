95a2a8e07289a9641615078d7a1cee8053b5f1b6
num-con <146140810+givd-bid@users.noreply.github.com>
2024-01-31 10:02:49 -0800

    Create click.js

---
+++ testing/click.js
@@ -0,0 +1,9 @@
+let x = Math.floor(Math.random() * 3)
+void console.log(x)
+
+if (x == 0) {
+  document.getElementById("html").style.backgroundImage = `url("assets/R.gif")`
+  
+} else{
+  document.getElementById("html").style.backgroundImage = `url("assets/cow-dancing.gif")`
+}
